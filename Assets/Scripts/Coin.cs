81abc35033e37abd51214c3a941f8b62de7182b5
commissionergordon <scottl318@gmail.com>
2018-04-18 15:35:28 -0400

    updates 0,0,0 for some 3d text, faster Crayon transitions, inconsistent starting scale tho

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Coin : MonoBehaviour {
+	
+	[SerializeField]
+	private float rotateSpeed = 1.0f; // In rotations per second
+
+	[SerializeField]
+	private float floatSpeed = 0.5f; // In cycles (up and down) per second
+
+	[SerializeField]
+	private float movementDistance = 0.5f; // The maximum distance the coin can move up and down
+
+	private float startingY;
+	private bool isMovingUp = true;
+	
+	void OnTriggerEnter(Collider collider)
+	{
+		if (collider.gameObject.tag == "Player")
+		{
+			Pickup();
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		startingY = transform.position.y + Random.Range (-1.5f, 1.5f);
+//		transform.Rotate (transform.up, Random.Range (0f, 360f));
+	}
+
+	void Update() {
+//		Spin ();
+		Float ();
+	}
+
+	private void Pickup()
+	{
+		Debug.Log ("Clicked!");
+//		GameManager.Instance.NumCoins++;
+//		Destroy (gameObject);
+	}
+
+	private void Spin()
+	{
+//		transform.Rotate (transform.up, 360 * rotateSpeed * Time.deltaTime);
+	}
+
+	private void Float()
+	{
+		float newY = transform.position.y + (isMovingUp ? 1 : -1) * 2 * movementDistance * floatSpeed * Time.deltaTime;
+		
+		if (newY > startingY + movementDistance)
+		{
+			newY = startingY + movementDistance;
+			isMovingUp = false;
+		}
+		else if (newY < startingY)
+		{
+			newY = startingY;
+			isMovingUp = true;
+		}
+		
+		transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+	}
+
+}
--- Assets/Scripts/FlowerController.cs
+++ Assets/Scripts/FlowerController.cs
@@ -49,8 +49,11 @@ public class FlowerController : MonoBehaviour {
         InitializePetalPositions(isThisFlowerOpenOnStart);
         InitializePetalsScale(isThisFlowerOpenOnStart);
 
-        // Set IsFlowerExpanded to isThisFlowerOpenOnStart
-        isFlowerExpanded = isThisFlowerOpenOnStart;
+
+		// Set IsFlowerExpanded to isThisFlowerOpenOnStart
+		isFlowerExpanded = isThisFlowerOpenOnStart;
+
+
     }
 
 
@@ -61,12 +64,12 @@ public class FlowerController : MonoBehaviour {
         {
             float petalAngleInDeg = initialAngleOriginInDeg + 360f / petals.Length * i;
             petalPositionsOpen[i] = AngleToVec(petalAngleInDeg) * petalMaxRadius;
-            //petalPositionsClosed[i] = new Vector3(0f, 0f, 0f);
+			// For some reason can't be 0, because it gets multiplied? 
             petalPositionsClosed[i] = petalPositionsOpen[i] * -1;
         }
 
         petalScaleOpen = new Vector3(FlowersManager.Instance.petalMaxScale, FlowersManager.Instance.petalMaxScale, 1f);
-        petalScaleClosed = new Vector3(0.2f, 0.2f, 1f);
+		petalScaleClosed = new Vector3(0.05f, 0.05f, 1f);
         //float invMaxPetalScale = 1f / FlowersManager.Instance.petalMaxScale;
         //petalScaleClosed = new Vector3(invMaxPetalScale, invMaxPetalScale, 1f);
     }
@@ -90,7 +93,6 @@ public class FlowerController : MonoBehaviour {
     }
     
 
-
     private Vector3 AngleToVec(float angleInDeg)
     {
         return new Vector3(Mathf.Cos(angleInDeg * Mathf.Deg2Rad), Mathf.Sin(angleInDeg * Mathf.Deg2Rad), 0);
@@ -103,14 +105,14 @@ public class FlowerController : MonoBehaviour {
         {
             if (isExpanding)
             {
-                //petals[i].SetRelativePosition(petalPositionsOpen[i], 1.2f, Easing.Custom, "0.34,0.89,0.71,-0.33");
-                petals[i].SetRelativePosition(petalPositionsOpen[i], 1.2f, Easing.Linear);
-                petals[i].transform.GetChild(0).gameObject.SetScale(petalScaleOpen, 1.2f);
+				petals[i].SetRelativePosition(petalPositionsOpen[i], 0.2f, Easing.Custom, "0.05,0.78,0.65,0.99");
+//                petals[i].SetRelativePosition(petalPositionsOpen[i], 0.2f, Easing.Linear);
+                petals[i].transform.GetChild(0).gameObject.SetScale(petalScaleOpen, 0.2f);
             }
             else
             {
-                petals[i].SetRelativePosition(petalPositionsClosed[i], 0.2f, Easing.Custom, "0.05,0.55,0.36,0.92");
-                petals[i].transform.GetChild(0).gameObject.SetScale(petalScaleClosed, 1.2f);
+                petals[i].SetRelativePosition(petalPositionsClosed[i], 0.2f, Easing.Custom, "0.05,0.78,0.65,0.99");
+                petals[i].transform.GetChild(0).gameObject.SetScale(petalScaleClosed, 0.2f);
             }
 
         }
